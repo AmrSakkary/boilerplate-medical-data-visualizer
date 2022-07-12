6f7ea060ad9830d1ab06159d5bad8c1b5db793ed
AmrGomaa <amrsakary1@gmail.com>
2022-07-12 02:31:38 +0200

    adding final plot

--- .ipynb_checkpoints/medical_data_visualizer-checkpoint.py
+++ .ipynb_checkpoints/medical_data_visualizer-checkpoint.py
@@ -29,7 +29,8 @@ df.gluc = df.gluc.apply(lambda x :norm_data(x))
 # Draw Categorical Plot
 def draw_cat_plot():
     # Create DataFrame for cat plot using `pd.melt` using just the values from 'cholesterol', 'gluc', 'smoke', 'alco', 'active', and 'overweight'.
-    df_cat =  df[['id', 'cardio', 'cholesterol','gluc', 'smoke', 'alco', 'active', 'overweight']].melt(id_vars=['id', 'cardio'])
+    df_cat =  df[['cardio', 'active', 'alco', 'cholesterol', 'gluc', 'overweight', 'smoke']].melt(id_vars=['cardio'])
+    
 
 
     # Group and reformat the data to split it by 'cardio'. Show the counts of each feature. You will have to rename one of the columns for the catplot to work correctly.
@@ -38,9 +39,9 @@ def draw_cat_plot():
     # Draw the catplot with 'sns.catplot()'
     cat_order = ['active', 'alco', 'cholesterol', 'gluc', 'overweight', 'smoke']
     
-    fig = sns.catplot(x='variable',hue = 'value',
-               data=df_cat, kind="count", col='cardio', order = cat_order, aspect = 1)
+    fig = sns.catplot(x='variable',hue = 'value',data=df_cat, kind="count", col='cardio', aspect = 1, order = cat_order)
     fig.set_ylabels('total')
+    fig = fig.fig
     
 
 
@@ -52,23 +53,54 @@ def draw_cat_plot():
 # Draw Heat Map
 def draw_heat_map():
     # Clean the data
-    df_heat = None
+#     df_heat = df[(df['ap_lo'] <= df['ap_hi'])]
+#     df_heat = df_heat.drop(columns = ['bmi'])
+#     # height is less than the 2.5th percentile (Keep the correct data with (df['height'] >= df['height'].quantile(0.025)))
+    
+#     df_heat = df_heat[(df_heat['height'] >= df_heat['height'].quantile(0.025))]
+    
+#     # height is more than the 97.5th percentile
+#     df_heat = df_heat[(df_heat['height'] <= df_heat['height'].quantile(0.975))]
+#     # weight is less than the 2.5th percentile
+    
+#     df_heat = df_heat[(df_heat['weight'] >= df_heat['weight'].quantile(0.025))]
+
+#     # weight is more than the 97.5th percentile
+#     df_heat = df_heat[(df_heat['weight'] <= df_heat['weight'].quantile(0.975))]
+
+    df_heat = df[(df['ap_lo']<=df['ap_hi']) &
+                    (df['height'] >= df['height'].quantile(0.025))&
+                    (df['height'] <= df['height'].quantile(0.975))&
+                    (df['weight'] >= df['weight'].quantile(0.025))&
+                    (df['weight'] <= df['weight'].quantile(0.975))
+                ]
+    df_heat = df_heat.drop(columns = ['bmi'])
+   
+    
 
     # Calculate the correlation matrix
-    corr = None
+    corr = df_heat.corr()
+    # corr = round(corr,1)
 
     # Generate a mask for the upper triangle
-    mask = None
+    mask = np.zeros_like(corr)
+    mask[np.triu_indices_from(mask)] = True
+    
+
 
 
 
     # Set up the matplotlib figure
-    fig, ax = None
+    with sns.axes_style("white"):
+        fig, ax = plt.subplots(figsize=(10, 10))
+        # Draw the heatmap with 'sns.heatmap()'
+        ax = sns.heatmap(corr, mask=mask, vmax=.3, square=True, annot=True, center = 0, fmt='.1f')
 
-    # Draw the heatmap with 'sns.heatmap()'
+    
 
 
 
     # Do not modify the next two lines
     fig.savefig('heatmap.png')
     return fig
+
